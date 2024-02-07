7622562b5a61a86fa00196961745127a15b03e1f
a-lambda <alain.corvisier@proton.me>
2024-02-07 16:38:35 +0100

    json to proceed next time

--- timeseries.R
+++ timeseries.R
@@ -3,7 +3,8 @@
 libs <- c(
   "ncdf4",
   "tidyverse",
-  "data.table"
+  "data.table",
+  "jsonlite"
 )
 #install missing libraries
 installed_libs <- libs %in% rownames(installed.packages())
@@ -12,6 +13,13 @@ if (any(installed_libs == FALSE)) {
 }
 #load libraries
 invisible(lapply(libs, library, character.only = TRUE))
+
+#weigths to apply to square of 0.25°x0.25° along the latitude
+area_weights <- fread(
+  file = "area_weights.csv",
+  dec = ","
+) |> 
+  pull( w )
 
 # get weighted mean of one variable 
 # between lat -60 and lat +60 from one NetCDF file
@@ -32,11 +40,7 @@ get_var_all_weighted_mean <- function(var) {
   files <- list.files(path = "DATA", pattern = "*.nc", full.names = TRUE)
   origin_date <- ymd("1978-01-01")
   wm_matrix <- matrix(numeric(0), nrow = length(files), ncol = 2)
-  area_weights <- fread(
-    file = "area_weights.csv",
-    dec = ","
-  ) |> 
-    pull( w )
+  colnames(wm_matrix) <- c("time", "wm")
   for (i in seq_along(files)) { 
     result <- get_var_weighted_mean(files[i], "sst")
     wm_matrix[i, 1] <- result[1]
@@ -49,3 +53,11 @@ get_var_all_weighted_mean <- function(var) {
   
 df_wm_sst <- get_var_all_weighted_mean("sst")
 saveRDS(df_wm_sst, "weighted_mean_sst.RDS")
+
+## load official json data to compare mine with
+
+link <- "https://climatereanalyzer.org/clim/sst_daily/json/oisst2.1_world2_sst_day.json"
+result <- jsonlite::fromJSON(link)
+
+
+
